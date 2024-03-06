82db9fa28c29fd6b8ad0e599d0563045f7591106
NimishaSabari <nimminimmi236@gmail.com>
2024-03-06 22:24:04 +0530

    1

---
+++ JAVA/new1.java
@@ -0,0 +1,25 @@
+package JAVA_OOPS_DS.JAVA;
+ //primitive datatypes :
+public class new1 {
+    public static void main(String[] args) {
+        //to store whole numbers : 4 datatypes
+        int age = 18; //datatype identifier =assigning value  range[-2billion ,2b]
+        //byte age = 18; range {-128, 127}
+        //short age = 32k; range{-32k ,32k}
+        //long age = 1000000000L; range {more than 2b}
+
+        //to store decimal numbers :
+        float marks = 95.6F;
+        double mark = 95.877777;
+        boolean presence = true - 1 false -0
+
+        //to store alphabet
+        char grade = 'A';
+
+        //task2
+        int count = 6;
+        System.out.println(age);
+        System.out.println(count);
+    }
+
+}
