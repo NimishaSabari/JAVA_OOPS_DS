f7ed319dd4b77ac69ce6924b3845e65f8d3176dd
NimishaSabari <nimminimmi236@gmail.com>
2024-03-06 22:41:55 +0530

    1

---
+++ JAVA/referencedatatype.java
@@ -0,0 +1,33 @@
+package JAVA_OOPS_DS.JAVA;
+
+public //reference datatype
+//primitive if there are two values theey will not be dependent on each otherr
+//reference -> dependent on each other (2 var ) : hence if we change one value both values will be changed to same
+
+import java.awt.*;
+import java.util.Date;
+
+public class new3 {
+    public static  void main(String[] args) {
+        //primitive:
+        int a = 10;
+        int b = a;
+        a = 5;
+        System.out.println(a);
+        System.out.println(b);
+
+        Date today = new Date(); //new :space alocate default
+        System.out.println(today);
+        //reference :
+        Point p1 = new Point(5,7);
+        Point p2 = p1;
+        System.out.println(p1);
+        p1.x =7;
+        System.out.println(p1);
+        System.out.println(p2);
+
+    }
+}
+ {
+    
+}
