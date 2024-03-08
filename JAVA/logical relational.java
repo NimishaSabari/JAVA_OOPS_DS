b52cec9ef90dbe3e7c60b6e3f21158e377ba8e57
NimishaSabari <nimminimmi236@gmail.com>
2024-03-08 22:29:39 +0530

    logical  and relational operators

---
+++ JAVA/logical relational.java
@@ -0,0 +1,38 @@
+public class logicarelational {
+    public class new5 {
+        public static void main(String[] args) {
+            //RELATIONAL OPERATORS :
+            //declaring
+            int a = 10;
+            int b = 5;
+            //6 -> o/p:
+            System.out.println(a<b);
+            System.out.println(a<=b);
+            System.out.println(a>b);
+            System.out.println(a>=b);
+            System.out.println(a==b);
+            System.out.println(a!=b);
+    
+            //LOGICAL OPERATORS :and && or|| not !
+            //not: -> opp answer
+            //or -> atleast one shld be true then true both false then false
+            //and -> if both true only true else false
+            boolean c = true;
+            boolean d = false;
+            boolean e = true;
+    
+            System.out.println(!c);
+            System.out.println(!d);
+            System.out.println(c||d);
+            System.out.println(c&&e);
+    
+            //priority precedence :
+            //division multiplication addition
+            //not and or 
+    
+    
+        }
+    }
+    
+    
+}
